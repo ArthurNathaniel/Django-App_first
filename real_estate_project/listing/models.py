8176b1018ed96c6b195365362478ba99405a6e19
Arthur Kwabena Larbi Nathaniel <105647591+ArthurNathaniel@users.noreply.github.com>
2023-03-30 05:51:53 -0500

    first commit

---
+++ real_estate_project/listing/models.py
@@ -0,0 +1,28 @@
+# # 
+# from django.db import models
+
+# class Listing(models.Model):
+#     title = models.CharField(max_length=150)
+#     price = models.IntegerField()
+#     num_bedrooms = models.IntegerField()
+#     num_bathrooms = models.IntegerField()
+#     square_footage = models.IntegerField()
+#     address = models.CharField(max_length=100)
+    
+#     def __str__(self):
+#         return self.title
+
+from django.db import models
+
+class Listing(models.Model):
+    title = models.CharField(max_length=150)
+    price = models.IntegerField()
+    num_bedrooms = models.IntegerField()
+    num_bathrooms = models.IntegerField()
+    square_footage = models.IntegerField()
+    address = models.CharField(max_length=100)
+    # image = models.ImageField(upload_to='listings/', null=True, blank=True)
+    
+    def __str__(self):
+        return self.title
+
